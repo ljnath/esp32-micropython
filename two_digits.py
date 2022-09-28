550858c7302b7d61e5cf60bd3e3ceb51be23b305
ljnath <lakhya.sci@gmail.com>
2022-09-29 03:13:38 +0530

    added script to display 2-digits number upto 99 in 2 LED display

--- two_digits.py
+++ two_digits.py
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-Micropython code to display 2 digits on to 2 7-segment LED display.
+Micropython code to display 2 digits on to 2 7-segment LED display (common cathode).
 Tested this code on ESP32
 
 Author: Lakhya Jyoti Nath
